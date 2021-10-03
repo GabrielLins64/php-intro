599cc60c26591cfc15fbe5160461511638a0b521
Gabriel Lins <gabrielinsmelo@gmail.com>
2021-10-03 13:00:37 -0300

    feat: add apache virtual hosts documentation

--- connection_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $conn = pg_connect ("host=localhost dbname=testdb user=gabriel password=poseidon");
-  if ($conn) {
-    echo "Connected!\n";
-  } else {
-    echo "There has been an error connecting!\n";
-  }
-?>
