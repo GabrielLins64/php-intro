74a048d71fd464f116126fa987dd856ba6af3255
Gabriel Lins <gabrielinsmelo@gmail.com>
2021-10-03 16:52:53 -0300

    feat: add constants lesson

--- cursophp.com/index.php
+++ cursophp.com/index.php
@@ -17,6 +17,7 @@
   echo "<a href='/tipos-de-dados.php'>Tipos de Dados</a><br>";
   echo "<a href='/aspas-concat.php'>Aspas e Concatenação</a><br>";
   echo "<a href='/escopo-de-variaveis.php'>Escopo de Variáveis</a><br>";
+  echo "<a href='/constantes.php'>Constantes</a><br>";
 ?>
 
 </body>
