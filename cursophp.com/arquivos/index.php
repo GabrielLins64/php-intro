680706145fe7935f7c52e61fb2665e756ce0173a
Gabriel Lins <gabrielinsmelo@gmail.com>
2021-10-17 19:02:06 -0300

    refactor: change order from read file example

--- cursophp.com/arquivos/index.php
+++ cursophp.com/arquivos/index.php
@@ -4,6 +4,47 @@
 // ---------- X ---------- X ---------- 
   echo "<h2>Manipulação de Arquivos</h2>";
 
+  echo "<hr><h3>Ler arquivo ('r'ead mode)</h3>";
+  echo '
+    <b>*Este modo de abertura permite ler o arquivo.</b><br><br>
+    $arquivo = "arquivo.txt";<br>
+    $tamanhoArquivo = filesize($arquivo);<br>
+    $arquivoAberto = fopen($arquivo, "r");<br>
+    <br>
+    while(!feof($arquivoAberto)):<br>
+    &emsp;  $linha = fgets($arquivoAberto, $tamanhoArquivo);<br>
+    &emsp;  echo nl2br($linha);<br>
+    endwhile;<br>
+    <br>
+    fclose($arquivoAberto);<br>
+  <br>';
+
+?>
+<form action="/arquivos" method="get">
+  <button type="submit" name="ler" value="true">Ler o arquivo</button>
+</form>
+<?php
+
+  function ler()
+  {
+    $arquivo = "arquivo.txt";
+    $tamanhoArquivo = filesize($arquivo);
+    $arquivoAberto = fopen($arquivo, "r");
+
+    while(!feof($arquivoAberto)):
+      $linha = fgets($arquivoAberto, $tamanhoArquivo);
+      echo nl2br($linha);
+    endwhile;
+
+    fclose($arquivoAberto);
+  }
+
+  if (isset($_GET['ler']) && $_GET['ler']):
+    echo "<div style='color: green;'> Resultado:<br>";
+    ler();
+    echo "</div><br>";
+  endif;
+
   echo "<hr><h3>Escrever conteúdo ('w'rite mode)</h3>";
   echo '
     <b>*Este modo de abertura de arquivo sobrescreve todo o conteúdo do mesmo.</b><br><br>
@@ -64,47 +105,6 @@
 
   if (isset($_GET['adicionar']) && $_GET['adicionar']):
     adicionar();
-  endif;
-
-  echo "<hr><h3>Ler arquivo ('r'ead mode)</h3>";
-  echo '
-    <b>*Este modo de abertura permite ler o arquivo.</b><br><br>
-    $arquivo = "arquivo.txt";<br>
-    $tamanhoArquivo = filesize($arquivo);<br>
-    $arquivoAberto = fopen($arquivo, "r");<br>
-    <br>
-    while(!feof($arquivoAberto)):<br>
-      $linha = fgets($arquivoAberto, $tamanhoArquivo);<br>
-      echo nl2br($linha);<br>
-    endwhile;<br>
-    <br>
-    fclose($arquivoAberto);<br>
-  <br>';
-
-?>
-<form action="/arquivos" method="get">
-  <button type="submit" name="ler" value="true">Ler o arquivo</button>
-</form>
-<?php
-
-  function ler()
-  {
-    $arquivo = "arquivo.txt";
-    $tamanhoArquivo = filesize($arquivo);
-    $arquivoAberto = fopen($arquivo, "r");
-
-    while(!feof($arquivoAberto)):
-      $linha = fgets($arquivoAberto, $tamanhoArquivo);
-      echo nl2br($linha);
-    endwhile;
-
-    fclose($arquivoAberto);
-  }
-
-  if (isset($_GET['ler']) && $_GET['ler']):
-    echo "<div style='color: green;'> Resultado:<br>";
-    ler();
-    echo "</div><br>";
   endif;
 
 ?>
