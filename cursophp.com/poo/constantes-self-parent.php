4bee0f11f5fd48f5920722351e71bee92ab7b08c
Gabriel Lins <gabrielinsmelo@gmail.com>
2021-10-31 16:30:52 -0300

    refactor: add const explaination comment

--- cursophp.com/poo/constantes-self-parent.php
+++ cursophp.com/poo/constantes-self-parent.php
@@ -8,6 +8,9 @@ echo '
 
 class Pessoa<br>
 {<br>
+&emsp;<span style="color: gray">// É possível definir valores constantes em cada classe permanecendo a mesma e imutável.<br></span>
+&emsp;<span style="color: gray">// Constantes diferem de variáveis normais, ao não usar o símbolo $ para declará-las ou usá-las. A visibilidade padrão de constantes de classe é public.<br></span>
+&emsp;<span style="color: gray">// O valor deve ser uma expressão constante, não podendo ser (por exemplo) uma variável, uma propriedade, ou uma chamada a uma função.<br></span>
 &emsp;const nome = "Gabriel";<br>
 }<br>
 <br>
@@ -36,6 +39,8 @@ echo "<div style='color: green'>Resultado:<br>";
 
 class Pessoa
 {
+  // É possível definir valores constantes em cada classe permanecendo a mesma e imutável. Constantes diferem de variáveis normais, ao não usar o símbolo $ para declará-las ou usá-las. A visibilidade padrão de constantes de classe é public.
+  // O valor deve ser uma expressão constante, não podendo ser (por exemplo) uma variável, uma propriedade, ou uma chamada a uma função.
   const nome = "Gabriel";
 }
 
